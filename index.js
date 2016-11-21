1e822bd5534ddc337a54bf31b8e186ce8f3eaa5c
jamilomar <jamilomar.falconiaguirre@gmail.com>
2016-11-21 18:37:12 -0500

    Creation of Cache Middleware

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./lib');
+let lib = require('./lib/middleware');
+
+
+const express = require('express')
+const app = express()
+let baseConnection = { "host":"127.0.0.1",
+          "port":6379 
+    };
+//error logger
+let logger  = {
+error :function (error)
+{
+        throw error;
+}
+};
+//connection settings for redis client
+const redisOptions = {
+configuration: 
+{
+    maxTime:null    
+},
+connection : baseConnection,
+logger:logger
+};
+
+cache = new lib(redisOptions);
+cache.initialize();
+app.use(cache.getMiddleware(120));
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(5000);
