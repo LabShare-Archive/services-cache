512eb0a587dd54cd9e0d889cd469e76e6c2dfa6b
JamilOmar <jamilomar@Jamils-MacBook-Pro.local>
2016-11-25 17:04:21 -0500

    Duration Parameter ,Docker File, new tests added

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
-let lib = require('./lib/middleware');
-
-
-const express = require('express')
-const app = express()
-let baseConnection = { "host":"127.0.0.1",
-          "port":6379 
-    };
-//error logger
-let logger  = {
-error :function (error)
+module.exports= 
 {
-        console.log(error);
+  Cache : require('./lib/cache'),
+  Middleware: require('./lib/middleware')
 }
-};
-//connection settings for redis client
-const redisOptions = {
-configuration: 
-{
-    maxTime:null    
-},
-connection : baseConnection,
-logger:logger
-};
-
-cache = new lib(redisOptions);
-cache.initialize();
-app.use(cache.getMiddleware(120));
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-});
-
-app.get('/getvalue/:key', function (req, res) {
-  //cache.addDisabledCache(req);   
-  res.send('Hello World!')
-});
-app.listen(5000);
